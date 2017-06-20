1f4c281ba7c8608f5260f4aa1ed298e891c99ff1
unrealwork <unrealwork@gmail.com>
2017-06-20 16:50:39 +0300

    fix addFirst

--- src/main/java/stackqueue/Deque.java
+++ src/main/java/stackqueue/Deque.java
@@ -10,151 +10,157 @@ import java.util.NoSuchElementException;
  * @param <Item> any class.
  */
 public class Deque<Item> implements Iterable<Item> {
-    private class Node {
-        private Item value;
-        private Node next;
-        private Node previous;
 
-        private Node(Item value) {
-            this.value = value;
-        }
+  private class Node {
+
+    private Item value;
+    private Node next;
+    private Node previous;
+
+    private Node(Item value) {
+      this.value = value;
     }
+  }
 
-    private int size = 0;
-    private Node head;
-    private Node tail;
+  private int size = 0;
+  private Node head;
+  private Node tail;
 
-    /**
-     * Construct an empty deque.
-     */
-    public Deque() {
+  /**
+   * Construct an empty deque.
+   */
+  public Deque() {
+  }
+
+  /**
+   * Is the deque empty?
+   *
+   * @return true if deque is empty.
+   */
+  public boolean isEmpty() {
+    return head == null;
+  }
+
+
+  /**
+   * Return the number of items on the deque.
+   *
+   * @return the value
+   */
+  public int size() {
+    return size;
+  }                       //
+
+  /**
+   * Add the item to thhe front.
+   */
+  public void addFirst(Item item) {
+    if (item == null) {
+      throw new NullPointerException();
     }
-
-    /**
-     * Is the deque empty?
-     *
-     * @return true if deque is empty.
-     */
-    public boolean isEmpty() {
-        return head == null;
+    size++;
+    Node node = new Node(item);
+    if (isEmpty()) {
+      head = node;
+      tail = node;
+    } else {
+      node.next = head;
+      head.previous = node;
+      head = node;
     }
+  }
 
-
-    /**
-     * Return the number of items on the deque.
-     *
-     * @return the value
-     */
-    public int size() {
-        return size;
-    }                       //
-
-    /**
-     * Add the item to thhe front.
-     *
-     * @param item
-     */
-    public void addFirst(Item item) {
-        if (item == null) {
-            throw new NullPointerException();
-        }
-        size++;
-        Node node = new Node(item);
-        if (isEmpty()) {
-            head = node;
-            tail = node;
-        } else {
-            node.next = head;
-            head = node;
-        }
+  public void addLast(Item item) {
+    if (item == null) {
+      throw new NullPointerException();
     }
-
-    public void addLast(Item item) {
-        if (item == null) {
-            throw new NullPointerException();
-        }
-        size++;
-        Node node = new Node(item);
-        if (isEmpty()) {
-            head = node;
-            tail = node;
-        } else {
-            node.previous = tail;
-            tail.next = node;
-            tail = node;
-        }
-    }          // add the item to the end
-
-    /**
-     * Remove and return the item from the front.
-     *
-     * @return front element
-     */
-    public Item removeFirst() {
-        if (isEmpty()) {
-            throw new NoSuchElementException();
-        }
-        size--;
-        Item result = head.value;
-        if (head == tail) {
-            head = null;
-            tail = null;
-        }
-        head = head.next;
-        head.previous = null;
-        return result;
+    size++;
+    Node node = new Node(item);
+    if (isEmpty()) {
+      head = node;
+      tail = node;
+    } else {
+      node.previous = tail;
+      tail.next = node;
+      tail = node;
     }
+  }          // add the item to the end
+
+  /**
+   * Remove and return the item from the front.
+   *
+   * @return front element
+   */
+  public Item removeFirst() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    size--;
+    Item result = head.value;
+    if (head == tail) {
+      head = null;
+      tail = null;
+    }
+    head = head.next;
+    head.previous = null;
+    return result;
+  }
 
 
-    /**
-     * Remove and return the item from the end.
-     *
-     * @return a reference to removed element
-     */
-    public Item removeLast() {
-        if (isEmpty()) {
-            throw new NoSuchElementException();
+  /**
+   * Remove and return the item from the end.
+   *
+   * @return a reference to removed element
+   */
+  public Item removeLast() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    size--;
+    Item result = tail.value;
+    tail = tail.previous;
+    tail.next = null;
+    return result;
+  }
+
+  /**
+   * Return an iterator over items in order from front to end
+   *
+   * @return iterator
+   */
+  public Iterator<Item> iterator() {
+    return new Iterator<Item>() {
+      private Node current = head;
+
+      @Override
+      public boolean hasNext() {
+        return current != null;
+      }
+
+      @Override
+      public Item next() {
+        if (current == null) {
+          throw new NoSuchElementException();
         }
-        size--;
-        Item result = tail.value;
-        tail = tail.previous;
-        tail.next = null;
-        return result;
-    }
+        final Item value = current.value;
+        current = current.next;
+        return value;
+      }
 
-    /**
-     * Return an iterator over items in order from front to end
-     *
-     * @return iterator
-     */
-    public Iterator<Item> iterator() {
-        return new Iterator<Item>() {
-            private Node current = head;
-
-            @Override
-            public boolean hasNext() {
-                return !isEmpty() && tail.next != null;
-            }
-
-            @Override
-            public Item next() {
-                return current.value;
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        };
-    }
+      @Override
+      public void remove() {
+        throw new UnsupportedOperationException();
+      }
+    };
+  }
 
 
-    /**
-     * Test client
-     *
-     * @param args client's arguments.
-     */
-    public static void main(String[] args) {
+  /**
+   * Test client
+   *
+   * @param args client's arguments.
+   */
+  public static void main(String[] args) {
 
-    }
+  }
 }
